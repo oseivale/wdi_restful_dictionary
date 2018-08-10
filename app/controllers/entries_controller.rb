33b5ebf47f363c6a9639a4251e5fbe6baf81e3a9
Valerie Osei <valerie.osei@gmail.com>
2018-08-09 22:22:07 -0400

    created first instance method in the controller to hadle the index

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,10 @@
+class EntriesController < ApplicationController
+
+  def index
+    # here we'll define some @instance_variables to store data from the database for the views to use
+    render :index
+  end
+
+
+
+end
